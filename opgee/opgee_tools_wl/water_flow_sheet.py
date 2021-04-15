400b91c95cc8211da04b3801d7135ea36518735f
Wennan <wennan@stanford.edu>
2021-04-15 09:53:57 -0700

    water function

--- opgee/opgee_tools_wl/water_flow_sheet.py
+++ opgee/opgee_tools_wl/water_flow_sheet.py
@@ -5,7 +5,24 @@ import gas_library as gas_lib
 import numpy as np
 ################################# Water specific gravity ##################################
 def GAMMA_W(W_TDS):
+    
     # W_TDS is concentration of dissolved solids(TDS) in unit mg/L
     
     # see OPGEE v3.0a Flow Sheet tab row 83
-    return 1 + W_TDS * 0.695 * 1e-6             
+    return 1 + W_TDS * 0.695 * 1e-6
+################################# Water formation volume factor ###############################
+def WVF():
+    # see OPGEE v3.0a Flow Sheet tab row 83
+    return 1                                                                                # m3/std-m3
+################################ Water density ################################################
+def RHO_W(W_TDS):
+    w_tds = GAMMA_W(W_TDS)
+
+def RHO_W(W_TDS):
+    """[summary]
+
+    :param variable1: [description]
+    :type variable1: [type]
+    :param variable2: [description]
+    :type variable2: [type]
+    """
