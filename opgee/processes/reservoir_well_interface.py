b6912d0f0302b98fd0eb5551f911c8d7648f9cd7
Wennan <wennan@stanford.edu>
2021-04-27 15:09:50 -0700

    Add get_field() and change data type when reading from attributes

--- opgee/processes/reservoir_well_interface.py
+++ opgee/processes/reservoir_well_interface.py
@@ -7,6 +7,7 @@ class ReservoirWellInterface(Process):
     def run(self, **kwargs):
         _logger.debug("stream")
         field = self.get_field()
+        N2 = float(field.attr("gas_comp_N2"))
         self.print_running_msg()
 
     def impute(self):
