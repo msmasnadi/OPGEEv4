69d1fc6b1ff15830fab350e0842ff46330d0e1f9
Richard Plevin <rich@plevin.com>
2021-05-13 13:51:05 -0700

    Add procs to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,13 @@ import pytest
 from opgee.config import getConfig
 from opgee.log import setLogLevels, configureLogs
 from opgee.model import ModelFile
+from opgee.process import Process
 from opgee.pkg_utils import resourceStream
-import opgee.processes
 
-@pytest.fixture
+class ProcA(Process): pass
+class ProcB(Process): pass
+
+@pytest.fixture(scope="session ")
 def configure_logging_for_tests():
     # Don't display routine diagnostic messages during tests
     getConfig()
@@ -13,13 +16,13 @@ def configure_logging_for_tests():
     configureLogs(force=True)
     return None
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def opgee_model(configure_logging_for_tests):
     s = resourceStream('etc/opgee.xml', stream_type='bytes', decode=None)
     mf = ModelFile('[opgee package]/etc/opgee.xml', stream=s)
     return mf.model
 
-@pytest.fixture
-def model(): # configure_logging_for_tests
+@pytest.fixture(scope="module")
+def test_model(configure_logging_for_tests):
     mf = ModelFile('files/test_model.xml', add_stream_components=False, use_class_path=False)
     return mf.model
