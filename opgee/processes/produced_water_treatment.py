7285fad146953a424ce714d2acd11717b4916be7
Richard Plevin <rich@plevin.com>
2021-05-13 17:24:12 -0700

    Commented out code using Oil instance since not working at the moment.

--- opgee/processes/produced_water_treatment.py
+++ opgee/processes/produced_water_treatment.py
@@ -1,6 +1,5 @@
 from ..log import getLogger
 from ..process import Process
-from ..stream import PHASE_LIQUID
 
 _logger = getLogger(__name__)
 
@@ -12,13 +11,13 @@ class ProducedWaterTreatment(Process):
         water_FVF = model.const("water-FVF")
 
         # collect the "gas_comp_*" attributes in a pandas.Series
-        gas_comp = field.oil.gas_comp
-        API = field.oil.API
-        gas_oil_ratio = field.oil.gas_oil_ratio
+        # gas_comp = field.oil.gas_comp
+        # API = field.oil.API
+        # gas_oil_ratio = field.oil.gas_oil_ratio
         # oil_comp =
 
-        N2 = gas_comp.N2
-        CO2 = gas_comp.CO2
+        # N2 = gas_comp.N2
+        # CO2 = gas_comp.CO2
 
         # examples of getting attribute values
         num_wells = field.attr('num_prod_wells')
