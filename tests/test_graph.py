ce206ef7c3c383d30ccb0830ae8102211c41955d
Richard Plevin <rich@plevin.com>
2021-05-17 13:56:45 -0700

    Simplify graph tests

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,44 +1,20 @@
 import pytest
 from opgee.error import CommandlineError
 
-# See what exception is raised
-def test_graph_classes1(opgee):
-    pathname = '/dev/null'
-    opgee.run(None, ['graph', '--classes', 'core', '--classes_output', pathname])
-    assert True
+DEVNULL = '/dev/null'
 
-def test_graph_field1(opgee):
-    pathname = '/tmp/graph.png'
-    opgee.run(None, ['graph', '--field', 'test', '--field_output', pathname])
-    assert True
-
-
-def test_graph_classes(opgee):
-    pathname = '/dev/null'
+@pytest.mark.parametrize(
+    "args", [
+        ['graph', '--classes', 'core', '--classes_output', DEVNULL],
+        ['graph', '--field', 'test', '--field_output', DEVNULL],
+        ['graph', '--hierarchy_output', DEVNULL],
+    ]
+)
+def test_graphing(opgee, args):
     try:
-        opgee.run(None, ['graph', '--classes', 'core', '--classes_output', pathname])
+        opgee.run(None, args)
         good = True
-    except Exception as e:
-        good = False
-
-    assert good
-
-def test_graph_field(opgee):
-    pathname = '/tmp/graph.png'
-    try:
-        opgee.run(None, ['graph', '--field', 'test', '--field_output', pathname])
-        good = True
-    except Exception as e:
-        good = False
-
-    assert good
-
-def test_graph_model(opgee):
-    pathname = '/dev/null'
-    try:
-        opgee.run(None, ['graph', '--hierarchy_output', pathname])
-        good = True
-    except Exception as e:
+    except Exception:
         good = False
 
     assert good
