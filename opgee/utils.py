7cae57a3c1446c36f3b851bed35cdaba3831d943
Richard Plevin <rich@plevin.com>
2021-05-26 12:27:41 -0700

    - Deprecated unused functions in utils.py

--- opgee/utils.py
+++ opgee/utils.py
@@ -8,12 +8,14 @@
 '''
 import argparse
 import os
-import re
-import subprocess
 import sys
-from contextlib import contextmanager
 
-from .config import pathjoin, unixPath
+# Deprecated
+#import re
+#import subprocess
+#from contextlib import contextmanager
+
+from .config import unixPath
 from .error import OpgeeException
 from .log import getLogger
 
@@ -348,37 +350,38 @@ def loadModuleFromPath(module_path, raiseError=True):
 
     return module
 
-def importFrom(modname, objname, asTuple=False):
-    """
-    Import `modname` and return reference to `objname` within the module.
-
-    :param modname: (str) the name of a Python module
-    :param objname: (str) the name of an object in module `modname`
-    :param asTuple: (bool) if True a tuple is returned, otherwise just the object
-    :return: (object or (module, object)) depending on `asTuple`
-    """
-    from importlib import import_module
-
-    module = import_module(modname, package=None)
-    obj    = getattr(module, objname)
-    return ((module, obj) if asTuple else obj)
-
-def importFromDotSpec(spec):
-    """
-    Import an object from an arbitrary dotted sequence of packages, e.g.,
-    "a.b.c.x" by splitting this into "a.b.c" and "x" and calling importFrom().
-
-    :param spec: (str) a specification of the form package.module.object
-    :return: none
-    :raises OpgeeException: if the import fails
-    """
-    modname, objname = spec.rsplit('.', 1)
-
-    try:
-        return importFrom(modname, objname)
-
-    except ImportError:
-        raise OpgeeException("Can't import '%s' from '%s'" % (objname, modname))
+# Deprecated
+# def importFrom(modname, objname, asTuple=False):
+#     """
+#     Import `modname` and return reference to `objname` within the module.
+#
+#     :param modname: (str) the name of a Python module
+#     :param objname: (str) the name of an object in module `modname`
+#     :param asTuple: (bool) if True a tuple is returned, otherwise just the object
+#     :return: (object or (module, object)) depending on `asTuple`
+#     """
+#     from importlib import import_module
+#
+#     module = import_module(modname, package=None)
+#     obj    = getattr(module, objname)
+#     return ((module, obj) if asTuple else obj)
+#
+# def importFromDotSpec(spec):
+#     """
+#     Import an object from an arbitrary dotted sequence of packages, e.g.,
+#     "a.b.c.x" by splitting this into "a.b.c" and "x" and calling importFrom().
+#
+#     :param spec: (str) a specification of the form package.module.object
+#     :return: none
+#     :raises OpgeeException: if the import fails
+#     """
+#     modname, objname = spec.rsplit('.', 1)
+#
+#     try:
+#         return importFrom(modname, objname)
+#
+#     except ImportError:
+#         raise OpgeeException("Can't import '%s' from '%s'" % (objname, modname))
 
 # Deprecated
 # def printSeries(series, label, header='', asStr=False):
