912dc8a36fc5060ec5b30ee6284885571ce2e40d
Richard Plevin <rich@plevin.com>
2022-05-09 17:14:27 -0700

    Add test for TRAVIS in getHomeDir()

--- opgee/config.py
+++ opgee/config.py
@@ -217,6 +217,8 @@ def getHomeDir():
         drive = drive or env.get('HOMEDRIVE') or 'C:'
         home = os.path.realpath(drive + path)
         home = home.replace('\\', '/')            # avoids '\' quoting issues
+    elif env.get('TRAVIS') == 'true':
+        home = os.getenv('HOME')
     else:
         home = env.get('OPGEE_HOME') or os.getenv('HOME')
 
