eec5397b837cee0d50ec268396c69c8164cf7fc7
Richard Plevin <rich@plevin.com>
2022-08-15 16:21:08 -0700

    Fixes to get exported field attributes working

--- opgee/thermodynamics.py
+++ opgee/thermodynamics.py
@@ -706,7 +706,7 @@ class Oil(AbstractSubstance):
         :return:(float) liquid fuel composition (unit = mol/kg)
         """
         low_bound = 4
-        high_bound = 47.3 # 45   # TODO: changed temporarily to handle exported CSV field definitions
+        high_bound = 58 # 45   # TODO: changed temporarily to handle exported CSV field definitions
 
         if API.m < low_bound or API.m > high_bound:
             raise ModelValidationError(f"{API.m} is less than {low_bound} or greater than {high_bound}")
