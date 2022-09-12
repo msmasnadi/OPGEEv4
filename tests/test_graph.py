534d3ecfc4fd9d82deb0b85333222bff0fdb0d60
Richard Plevin <rich@plevin.com>
2022-09-12 12:34:40 -0700

    Make test for "sherlock", not linux (so tests run in CI environment)

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,12 +1,12 @@
-import platform
+import os
 import pytest
 from opgee.error import CommandlineError
 from opgee.config import IsWindows
 DEVNULL = 'nul' if IsWindows else '/dev/null'
 
-is_linux = platform.system() == 'Linux'
+is_sherlock = os.environ.get('LMOD_SYSHOST') == 'sherlock'
 
-@pytest.mark.skipif(is_linux, reason="requires the graphviz/dot which isn't working on sherlock")
+@pytest.mark.skipif(is_sherlock, reason="requires the graphviz/dot which isn't working on sherlock")
 @pytest.mark.parametrize(
     "args", [
         ['graph', '--classes', 'core', '--classes-output', DEVNULL],
@@ -24,7 +24,7 @@ def test_graphing(opgee, args):
 
     assert good
 
-@pytest.mark.skipif(is_linux, reason="requires the graphviz/dot which isn't working on sherlock")
+@pytest.mark.skipif(is_sherlock, reason="requires the graphviz/dot which isn't working on sherlock")
 def test_unknown_field(opgee):
     with pytest.raises(CommandlineError, match=r"Field name .* was not found in model"):
         opgee.run(None, ['graph', '--field', 'unknown-field'])
