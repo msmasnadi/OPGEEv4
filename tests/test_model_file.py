2f5eecf028c09d6e10f0c7f515202b6200a088a8
Richard Plevin <rich@plevin.com>
2023-04-07 11:09:06 -0700

    Added support for running fields in very large model files

--- tests/test_model_file.py
+++ tests/test_model_file.py
@@ -1,7 +1,7 @@
 import pytest
 from opgee.error import OpgeeException
-from opgee.model_file import ModelFile
-from .utils_for_tests import path_to_test_file # , load_test_model
+from opgee.model_file import (ModelFile, fields_for_analysis, extracted_model)
+from .utils_for_tests import path_to_test_file
 
 def test_no_file():
     pathnames = []
@@ -11,3 +11,21 @@ def test_no_file():
 def test_modifies():
     xml_path = path_to_test_file('test_fields.xml')
     mf = ModelFile(xml_path, use_default_model=True)
+
+def test_many_fields():
+    analysis_name = 'test-fields'
+    model_xml = path_to_test_file('test-fields-9000.xml')
+
+    field_names = fields_for_analysis(model_xml, analysis_name)
+    assert len(field_names) == 8966
+
+    analyses = [analysis_name]
+
+    for field_name, xml_pathname in extracted_model(model_xml, analysis_name, field_names=field_names[:4]):  # extract the first 4 only
+        # We just test that the models load
+        mf = ModelFile(xml_pathname, add_stream_components=False, use_class_path=False,
+                       use_default_model=True, save_to_path=None,
+                       analysis_names=analyses,  # may be unnecessary
+                       field_names=[field_name])        # may be unnecessary
+
+
