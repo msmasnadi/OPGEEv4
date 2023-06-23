28040acb2b5c07b3759b0208aa065cca86901ca6
Wennan <wennan@stanford.edu>
2023-06-23 11:31:15 -0700

    test smart default using opgee.xml

--- tests/test_opgee-xml.py
+++ tests/test_opgee-xml.py
@@ -1,4 +1,5 @@
 import pytest
+from opgee import ureg
 from opgee.model_file import ModelFile
 
 @pytest.fixture(scope="module")
@@ -15,3 +16,13 @@ def test_gas_lifting_field(opgee, field_name):
 
     # Just testing that we can run the fields without error
     field.run(analysis)
+
+# TODO: This is a temporary test just for debugging smart defaults. Delete this test after solving the smart defaults issue.
+@pytest.mark.parametrize(
+    "field_name", [ ('gas_lifting_field')])
+def test_smart_default(opgee, field_name):
+    analysis = opgee.get_analysis('example')
+    field = analysis.get_field(field_name)
+    T = field.attr('prod_water_inlet_temp')
+
+    assert T == ureg.Quantity(340, 'degF')
