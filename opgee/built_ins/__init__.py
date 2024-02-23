356ea5c59f9ca0c78c9740788f202c7b7503f67e
Richard Plevin <rich@plevin.com>
2024-02-23 10:29:38 -0800

    Created update subcommand to revise model XML files to use <FieldRef> in <Analysis> elements. Updated several etc/test-*.xml files.

--- opgee/built_ins/__init__.py
+++ opgee/built_ins/__init__.py
@@ -7,9 +7,7 @@ from .run_plugin import RunCommand
 from .csv2xml_plugin import Csv2XmlCommand
 from .merge_plugin import MergeCommand
 from .gensim_plugin import GensimCommand
-#from .runsim_plugin import RunsimCommand
-#from .genwor_plugin import GenworCommand
-#from .runmany_plugin import RunManyCommand
+from .update_plugin import UpdateCommand
 
 BuiltinSubcommands = [
     CollectCommand,
@@ -17,11 +15,9 @@ BuiltinSubcommands = [
     ConfigCommand,
     Csv2XmlCommand,
     GensimCommand,
-    # GenworCommand
     GraphCommand,
     GUICommand,
     MergeCommand,
     RunCommand,
-    # RunManyCommand,
-    # RunsimCommand,
+    UpdateCommand,
 ]
