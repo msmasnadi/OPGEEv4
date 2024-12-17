5d7c562daa278fb28d4d65b0bc2a20ca27f3cac5
Michael Barlow <mbarlow@rmi.org>
2024-12-17 10:46:43 -0700

    remove post processor cli args

--- opgee/built_ins/run_plugin.py
+++ opgee/built_ins/run_plugin.py
@@ -101,24 +101,24 @@ class RunCommand(SubcommandABC):
             help="""Comma-delimited list of field names to exclude from analysis""",
         )
 
-        parser.add_argument(
-            "-g",
-            "--post-proc-plugin",
-            metavar="PATH",
-            action="append",
-            default=[],
-            help="""Loads the post-processing plugin from the given path. The file
-                    must contain a single, valid subclass of ``opgee.post_process.PostProcess``.
-                    This arg can be specified multiple times to load multiple post-processing
-                    classes. They will be invoked in the order given on the command-line.""")
+        # parser.add_argument(
+        #     "-g",
+        #     "--post-proc-plugin",
+        #     metavar="PATH",
+        #     action="append",
+        #     default=[],
+        #     help="""Loads the post-processing plugin from the given path. The file
+        #             must contain a single, valid subclass of ``opgee.post_process.PostProcess``.
+        #             This arg can be specified multiple times to load multiple post-processing
+        #             classes. They will be invoked in the order given on the command-line.""")
 
-        parser.add_argument(
-            "-G",
-            "--no-post-proc-plugin-path",
-            action="store_true",
-            help="""Override the automatic loading of post-processing plugins from the path(s)
-                specified in config variable OPGEE.PostProcPluginPath. Specific plugins can
-                still be loaded using the --post-plugin argument.""")
+        # parser.add_argument(
+        #     "-G",
+        #     "--no-post-proc-plugin-path",
+        #     action="store_true",
+        #     help="""Override the automatic loading of post-processing plugins from the path(s)
+        #         specified in config variable OPGEE.PostProcPluginPath. Specific plugins can
+        #         still be loaded using the --post-plugin argument.""")
 
         parser.add_argument(
             "-i",
@@ -358,15 +358,6 @@ class RunCommand(SubcommandABC):
             packets = FieldPacket.packetize(
                 model_xml_file, analysis_name, field_names, packet_size
             )
-
-        # Load all plugins specified in OPGEE.PostProcPluginPath, unless user
-        # had overridden this using command line option --no-post-plugin-path
-        if not args.no_post_proc_plugin_path:
-            PostProcessor.load_all_plugins()
-
-        # Load explicitly specified post-processing plugins
-        for path in args.post_proc_plugin:
-            PostProcessor.load_plugin(path)
 
         results_list = []
         save_batches = batch_size is not None
